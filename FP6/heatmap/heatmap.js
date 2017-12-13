bcf7297305836dcedd694ef785da3f5515cfda6a
Jake Rourke <jrourke@bowdoin.edu>
2017-12-13 15:16:32 -0500

    updates to heatmap and fp13

--- FP6/heatmap/heatmap.js
+++ FP6/heatmap/heatmap.js
@@ -26,6 +26,8 @@ NS.colors = ['#dc143c','#c70a4e','#b2025f','#9b0070','#800080','#7a368d',
             '#70539a','#616ba7','#4682b4']
 // https://gka.github.io/palettes/#colors=crimson,purple,steelblue|steps=9|bez=0|coL=0
 
+NS.civil_list = ["Civil Liberty", "Other"]
+
 NS.issueAreas = [
   "Criminal Procedure",    // 1
   "Civil Rights",          // 2
@@ -87,15 +89,111 @@ function aggregateData() {
     })
   })
 
-  splitData = []
+  NS.civilLiberties = []
+  NS.otherGroup = []
 
+
+/*
   NS.dataGood.forEach(function (d) {
-    console.log(d.justiceName)
+    if (d.issueArea >= 0 && d.issueArea <= 5) {
+        NS.civilLiberties.push(d);
+    }
+
+    else {
+      NS.otherGroup.push(d);
+    }
 
   })
+*/
+
+  NS.dataGood.forEach(function (d) {
+    if (d.issueArea >= 0 && d.issueArea <= 5) {
+        d["typeX"] = "Civil Liberty";
+        d["typeNum"] = 0;
+    }
+
+    else {
+        d["typeX"] = "Other";
+        d["typeNum"] = 1;
+    }
+  });
 
 
-}
+
+  NS.civilDataGood = [];
+
+  var justiceCount = 0;
+
+  //change the data to 37x2 data points for civil liberties view
+  NS.dataByJustice.forEach(function (d) {
+
+    var count = 0;
+    var civil = [];
+    var other = [];
+
+    //get values into seperate lists
+    d.value.forEach(function (d) {
+      if (count <= 5) {
+        civil.push(d);
+      }
+
+      else {
+        other.push(d);
+      }
+
+      count++;
+
+
+    });
+
+    //counts for missing data
+    var civilMissing = 0;
+    var otherMissing = 0;
+
+
+    //get averages for each
+    var civilTotal = 0;
+    civil.forEach(function (d) {
+      if (d == "NaN") {
+        civilMissing++;
+        return;
+      }
+
+      civilTotal += Number(d);
+    });
+
+    var otherTotal = 0;
+    other.forEach(function (d) {
+      if (d == "NaN") {
+        otherMissing++;
+        return;
+      }
+      if(d == "0.00") {
+        otherMissing++;
+        return;
+      }
+
+
+      otherTotal += Number(d);
+    });
+
+    //console.log(civilTotal)
+    console.log(other)
+
+
+    var civilVal = civilTotal / (6 - civilMissing);
+    var otherVal = otherTotal / (8 - otherMissing);
+
+    NS.civilDataGood.push({ direction: civilVal, justiceName: justiceCount, typeNum: 0});
+    NS.civilDataGood.push({ direction: otherVal, justiceName: justiceCount, typeNum: 1});
+
+    justiceCount++;
+
+
+  });
+
+
+};
 
 
 function main () {
@@ -108,10 +206,13 @@ function main () {
   svg = d3.select("body").append("svg")
           .attr("width", NS.width + NS.margin.left + NS.margin.right)
           .attr("height", NS.height + NS.margin.top + NS.margin.bottom)
+          .attr("id", "heatmap")
           .append("g")
           .attr("transform", "translate(" + NS.margin.left + "," + NS.margin.top + ")");
 
-  heatmap(svg)
+  
+
+  heatmap_2(svg);
 }
 
 function heatmap(svg) {
@@ -153,6 +254,9 @@ function heatmap(svg) {
       return "#FFFFFF";
   }
 
+
+
+
   cards.enter().append("rect")
       .attr("x", function(d) {
         return d.issueArea * NS.gridSize;
@@ -187,6 +291,136 @@ function heatmap(svg) {
         .text((d) => "≥ " + Math.round(d))
         .attr("x", (d, i) => NS.legendElementWidth * i)
         .attr("y", NS.height + NS.gridHeight);
+
+} // end heatmap
+
+function heatmap_2(svg) {
+  console.log("here");
+  colorScale = d3.scaleQuantile()
+    .domain([1, 2])
+    .range(NS.colors);
+
+
+
+  tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return d.direction; });
+
+  svg.call(tip);
+
+  cards = svg.selectAll(".typeNum")
+      .data(NS.civilDataGood)
+
+  justiceLabels = svg.selectAll(".justiceLabel")
+    .data(NS.justices)
+    .enter().append("text")
+      .text(function (d) { return d; })
+      .attr("x", 0)
+      .attr("y", (d, i) => i * NS.gridHeight)
+      .style("text-anchor", "end")
+      .attr("transform", "translate(-6," + NS.gridHeight / 1.5 + ")")
+      .attr("class", (d, i) => ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"));
+
+  issueLabels = svg.selectAll(".issueLabel")
+    .data(NS.civil_list)
+    .enter().append("text")
+      .text(function (d) { return d; })
+      .attr("x", (d, i) => i * NS.gridSize)
+      .attr("y", 0)
+      .style("text-anchor", "middle")
+      .attr("transform", "translate(" + NS.gridSize / 2 + ", -6)")
+      .attr("class", (d, i) => ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"));
+
+
+  cards.append("title");
+
+  function filterColors(dir) {
+    if(dir > 0)
+      return colorScale(dir);
+    else
+      return "#FFFFFF";
+  }
+
+  console.log("got here!!")
+
+
+  cards.enter().append("rect")
+      .attr("x", function(d) {
+      
+        return d.typeNum * NS.gridSize;
+      })
+      .attr("y", function(d) {
+        return d.justiceName * NS.gridHeight;
+      })
+      .attr("rx", 4)
+      .attr("ry", 4)
+      .attr("class", "issueArea bordered")
+      .attr("width", NS.gridSize)
+      .attr("height", NS.gridHeight)
+      .style("fill", function(d) {
+        return filterColors(d.direction);
+      })
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide);
+
+      console.log("Gothere22!!!")
+
+      /*
+
+      legend = svg.selectAll(".legend")
+          .data([0].concat(colorScale.quantiles()), (d) => d);
+
+      legend_g = legend.enter().append("g")
+          .attr("class", "legend");
+
+      legend_g.append("rect")
+        .attr("x", (d, i) => NS.legendElementWidth * i)
+        .attr("y", NS.height)
+        .attr("width", NS.legendElementWidth)
+        .attr("height", NS.gridHeight / 2)
+        .style("fill", (d, i) => NS.colors[i]);
+
+      legend_g.append("text")
+        .attr("class", "mono")
+        .text((d) => "≥ " + Math.round(d))
+        .attr("x", (d, i) => NS.legendElementWidth * i)
+        .attr("y", NS.height + NS.gridHeight);
+
+      */
+
+} // end heatmap
+
+
+//Remove SVG before creating new heatmap
+function removeHSVG() {
+  d3.select("#heatmap").remove();
+
+} // end removeSVG
+
+
+function civilButton() {
+  removeHSVG();
+  // make SVG
+  svg = d3.select("body").append("svg")
+          .attr("width", NS.width + NS.margin.left + NS.margin.right)
+          .attr("height", NS.height + NS.margin.top + NS.margin.bottom)
+          .attr("id", "heatmap")
+          .append("g")
+          .attr("transform", "translate(" + NS.margin.left + "," + NS.margin.top + ")");
+
+  heatmap_2(svg);
+
+}
+
+function regularButton() {
+
+  removeHSVG();
+  svg = d3.select("body").append("svg")
+          .attr("width", NS.width + NS.margin.left + NS.margin.right)
+          .attr("height", NS.height + NS.margin.top + NS.margin.bottom)
+          .attr("id", "heatmap")
+          .append("g")
+          .attr("transform", "translate(" + NS.margin.left + "," + NS.margin.top + ")");
+
+  heatmap(svg);
 
 }
 
